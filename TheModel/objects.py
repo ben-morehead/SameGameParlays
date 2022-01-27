b3ff14d87b4f822e1f6c465e1cc91796ab87b6ea
ben-morehead <ben.morehead1221@gmail.com>
2022-01-27 12:36:40 -0500

    Added the acquisition of datapoints for a player

--- TheModel/objects.py
+++ TheModel/objects.py
@@ -10,7 +10,6 @@ import torchvision
 import random
 import time
 from datetime import date
-import requests
 import sqlite3
 import logging
 
@@ -36,6 +35,8 @@ class Helper():
 
 class DataFormatter():
     def __init__(self):
+        #Note: For RNN If the data formatting doesn't work out, we can make a self.max_size = 30 as a safety.
+
         self.today = date.today().strftime("%d/%m/%y")
         self.game_path = "player_games.json"
         self.db_table_name = "gamedetails"
@@ -66,6 +67,7 @@ class DataFormatter():
             self.player_games[str(player_id)] = self.get_player_history(player_id)
         game_list = self.player_games[str(player_id)]
         game_list = list(map(int, self.player_games[str(player_id)]))
+        label = None
 
         connection = sqlite3.connect('gamedata.db')
         cursor = connection.cursor()
@@ -78,22 +80,57 @@ class DataFormatter():
         #Determine whether grabbing random set of games or just the most recent(depending on if we want to sample currently or use dataformatting)
         if most_recent is True:
             print("Most Recent {}".format(num_games))
-            index = 0
+            #index = 1 #IF GAME OF DAY IS GIVING ISSUE
+            index = 0 #OTHERWISE
         else:
             print("Random {}".format(num_games))
             index=random.randint(0, len(game_list) - num_games + 1)
             label_game = game_list[index-1]
+            cursor.execute(f"SELECT PTS FROM {self.db_table_name} WHERE GAME_ID={label_game} AND PLAYER_ID={player_id}")
+            print(label_game)
+            points = cursor.fetchone()[0]
+            label_index, _ = self.points_to_label(points)
+            print("Points in Next Game: {}".format(points))
+            print("Label for Next Game Points: {}".format(label_index))
         
         #Prepare the game list for getting db data
         game_sublist = game_list[index:(index + num_games)]
         game_sublist.reverse()
 
-        #Get database data
+        #Get database data and 0-pad to ensure stackability
+        full_seq = []
+        max_size = 0
         for game_id in game_sublist:
-            game_df = self.get_game_from_db(cursor, game_id)
+            game_arr = self.get_game_from_db(cursor, game_id)
+            full_seq.append(game_arr)
+            if game_arr.shape[0] > max_size: max_size = game_arr.shape[0]
+
+        for i,score in enumerate(full_seq):
+            rows_to_add = max_size - score.shape[0]
+            if rows_to_add > 0:
+                zero_arr = np.zeros([rows_to_add, score.shape[1]])
+                full_seq[i] = np.append(score, zero_arr, axis=0)
+
+        #Reshaping for datapoint purposes
+        datapoint = np.stack(full_seq)
+        datapoint = np.reshape(datapoint, [datapoint.shape[0], datapoint.shape[1] * datapoint.shape[2]])
         
         cursor.close()
         connection.close()
+
+        #Return Statement
+        if most_recent: return datapoint
+        else: return datapoint, label_index
+
+    def points_to_label(self, points):
+        label = np.zeros([11])        
+        if points >= 50:
+            index = -1
+        else:
+            index = int(np.floor(points / 5))
+        label[index] = 1
+        label.astype(float)
+        return index, label
 
     def get_player_id(self, player_name):
         player_id = players.find_players_by_full_name(player_name)[0]["id"]
@@ -155,7 +192,7 @@ class DataFormatter():
         return ret
 
     def get_game_from_db(self, cursor, game_id):
-        get_cmd = f"SELECT * FROM {self.db_table_name} WHERE GAME_ID={game_id}"
+        get_cmd = f"SELECT * FROM {self.db_table_name} WHERE GAME_ID={game_id};"
         cursor.execute(get_cmd)
         select_res = cursor.fetchall()
         full_array = np.delete(np.array(select_res), obj=0, axis=1)
@@ -213,7 +250,7 @@ class DataFormatter():
             row_str = ", ".join(row_list)
             sql_cmd = f"INSERT INTO {self.db_table_name} ({field_string}) VALUES ({row_str});"
             cursor.execute(sql_cmd)
-
+        
         return 1
 
     def get_todays_games(self, game_id):
@@ -247,22 +284,15 @@ def test():
     dataf = DataFormatter()
     #Getting NBA Data:
     player_name = "Devin Booker"
-    seq_len = 1
+    seq_len = 5
     player_season = "2021-22"
     player_id = players.find_players_by_full_name(player_name)[0]["id"]
-    #available_seasons = commonplayerinfo.CommonPlayerInfo(player_id=player_id).available_seasons.get_data_frame()
-    #player_game_log = playergamelog.PlayerGameLog(player_id=player_id, season=player_season).get_data_frames()[0]
-    #sample_game_id = player_game_log.loc[0,"Game_ID"]
-    
-    print("Player Name: {} | Player ID: {}".format(player_name, player_id))
-    print("BOX SCORE\n-----------------")
-    ret = dataf.get_player_datapoint(player_id, seq_len, most_recent=True)
-    #os.remove("gamedata.db")
-    #ret = dataf.inititalize_database(player_id=player_id)
-    #ret = dataf.update_player_db(player_id = player_id)
 
+    print("Player Name: {} | Player ID: {}".format(player_name, player_id))
+    print("-----------------")
+    ret = dataf.get_player_datapoint(player_id, seq_len, most_recent=False)
     #ret = dataf.get_player_tensor(player_id)
-    print(ret)
+    print(ret[0].shape, ret[1])
 
 
 
